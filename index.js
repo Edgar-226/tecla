7776d5c9f1ab88b2e7450fd8321c0c9fdf9a8455
Edgar-226 <80384879+Edgar-226@users.noreply.github.com>
2021-08-20 00:00:26 -0500

    actualizacion de html y Js

---
+++ index.js
@@ -0,0 +1,33 @@
+// function llamada (){
+//     console.log('Hola mundo');
+// }
+
+
+// function llamadora(call){
+//     console.log("Dentro de esta funcion");
+//     call()
+// }
+
+// llanadora(llamada);
+
+setTimeout(()=>{
+    console.log('1')
+}, 300)
+
+
+console.log('2')
+
+setTimeout(()=>{
+    console.log('3')
+}, 200)
+
+console.log('4')
+
+setTimeout(()=>{
+    console.log('6')
+}, 200)
+
+console.log('7')
+
+
+
